9afd33478fee5d04ce4f8dd87ed872b81476f48d
CoderQudratov <coderqudratov@gmail.com>
2024-12-20 00:24:49 +0500

    create firts project

---
+++ hello world/HelloWorld.java
@@ -0,0 +1,8 @@
+//Hello world klassi bu javadagi eng oddiy dastur
+public class HelloWorld {
+    //dastur ishlaganda shu method ishga tushadi
+    public static void main(String[] args) {
+        //bu ekranga "Hello world" matnini chiqaradi
+        System.out.println("Hello world");
+    }
+}
