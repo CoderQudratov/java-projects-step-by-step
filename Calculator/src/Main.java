9ed085c5c9c54d2a179c7a2698630bdda660e50a
CoderQudratov <coderqudratov@gmail.com>
2024-12-20 10:56:23 +0500

    get user in number

--- Calculator/src/Main.java
+++ Calculator/src/Main.java
@@ -3,6 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in); //Reading system input
+          // get user in number
+        System.out.println("Birinchi raqamni kiriting");
+        double firstNumber =scanner.nextDouble();
+
+        System.out.println("Ikkinchi raqamni kiriting");
+        double secondNumber =scanner.nextDouble();
+        
 
     }
 }
