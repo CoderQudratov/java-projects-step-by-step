74ca325c0e475644ffc2158d22d3c8cb8f21613f
CoderQudratov <coderqudratov@gmail.com>
2024-12-20 11:03:07 +0500

    calculating condtion

--- Calculator/src/Main.java
+++ Calculator/src/Main.java
@@ -12,7 +12,35 @@ public class Main {
 
         System.out.println("Amalni tanlang (+,-,*,/): ");
         char operation =scanner.next().charAt(0);
-        
+
+        double result;
+
+        switch (operation){
+            case '+':
+                result=firstNumber+secondNumber;
+                System.out.println("Natija: "+result);
+                break;
+            case '-':
+                result=firstNumber-secondNumber;
+                System.out.println("Natija: "+result);
+                break;
+            case '*':
+                result=firstNumber*secondNumber;
+                System.out.println("Natija: "+result);
+                break;
+            case '/':
+                if (secondNumber!=0){
+                    result=firstNumber/secondNumber;
+                    System.out.println("Natija: "+result);
+                }else{
+                    System.out.println("Nolga bolish mumkin emas");
+                }
+                break;
+            default:
+                System.out.println("Notug'ri amal kiritildi");
+
+        }
+        scanner.close();
 
     }
 }
